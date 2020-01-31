0ba349094c045a3aa59d4804692cb53fc83321b0
har4enko <pan4enko48@gmail.com>
2020-01-31 11:44:02 +0200

    HomeWork#1 - done

--- main.js
+++ main.js
@@ -14,9 +14,21 @@ document.writeln("Середня ціна: ", (sum/3).toFixed(2),".", "<br \\/>"
 let discount = (Math.random().toFixed(2));
 document.writeln("Сума до оплати: ", (sum-sum*discount).toFixed(2),", знижка складає: ", discount*100, "% - за умови покупки всіх трьох товарі", ".", "<br \\/>");
 if (discount > 0.5) {
-    document.writeln("Втрачена вигода: ", (sum * discount - sum / 2).toFixed(2));
+    document.writeln("Втрачена вигода: ", (sum * discount - sum / 2).toFixed(2), "<br \\/>");
 } else if (discount < 0.5) {
-    document.writeln("Прибуток склав: ", (sum - sum * discount - sum / 2).toFixed(2));
+    document.writeln("Прибуток склав: ", (sum - sum * discount - sum / 2).toFixed(2), "<br \\/>");
 } else {
-    document.writeln("Товар продано по собівартості")
+    document.writeln("Товар продано по собівартості", "<br \\/>")
 }
+document.writeln("Завдання категорії Advanced", "<br \\/>");
+document.writeln(`
+    Максимальне значення: ${Math.max(apples,oranges,bananas)}.<br \\\\/>
+    Мінімальне значення: ${Math.min(apples,oranges,bananas)}.<br \\\\/>
+    Сума всіх товарів: ${sum}.<br \\\\/>
+    Сума всіх цілих вартостей: ${Math.floor(apples)+Math.floor(oranges)+Math.floor(bananas)}.<br \\\\/>
+    Сума всіх товарів, заокруглена до сотень: ${Math.ceil(sum/100)*100}.<br \\\\/>
+    Чи є ціла сума всіх вартостей парною: ${Math.floor(sum)%2==0}.<br \\\\/>
+    Здача при покупці на 500 грн.: ${500-sum}.<br \\\\/>
+    Середня ціна: ${(sum/3).toFixed(2)}.<br \\\\/>
+`);
+
